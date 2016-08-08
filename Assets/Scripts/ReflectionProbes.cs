54191bba18c7bfa4a135e62edb737d7f52b857c8
Daniel Gleason <damianscape@gmail.com>
2016-08-07 22:51:10 -0400

    Reflection Probes are now scripted instead of being rendered every frame.

---
+++ Assets/Scripts/ReflectionProbes.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReflectionProbes : MonoBehaviour
+{
+
+    ReflectionProbe probe;
+    Vector3 lastPlayerPos;
+    GameObject player;
+
+    void Start()
+    {
+        probe = GetComponent<ReflectionProbe>();
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+
+        Collider[] collisions = Physics.OverlapSphere(transform.position, 3);
+
+        foreach (Collider other in collisions)
+        {
+            if (other.name == "Player")
+            {
+                probe.RenderProbe();
+            }
+        }
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, 3);
+    }
+
+}
