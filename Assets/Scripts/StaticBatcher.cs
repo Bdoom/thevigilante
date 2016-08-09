088a67a8c68784d5e70d5322359132bcb86db1d5
Colt-Zero <nardinibooks.man@hotmail.com>
2016-08-08 21:45:19 -0400

    Optimized Stuff

---
+++ Assets/Scripts/StaticBatcher.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticBatcher : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Awake()
+    {
+        StaticBatchingUtility.Combine(gameObject);
+	}
+}
---
+++ Assets/Scripts/Temporary/PrefabCreator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class PrefabCreator : MonoBehaviour
+{
+
+#if UNITY_EDITOR
+    void Start()
+    {
+        GameObject preFab = new GameObject();
+        preFab.name = transform.name;
+        preFab.transform.position = transform.position;
+
+        foreach (Transform copy in transform)
+        {
+            GameObject child = Instantiate(Resources.Load("Bench") as GameObject);
+            child.transform.parent = preFab.transform;
+            child.transform.position = copy.position;
+            child.transform.rotation = copy.rotation;
+            child.transform.localScale = copy.localScale;
+            child.name = "Bench";
+            // Set child position, rotation, scale, and other properties
+        }
+
+
+        PrefabUtility.CreatePrefab("Assets/Prefabs/" + transform.name + ".prefab", preFab);
+    }
+#endif
+}
